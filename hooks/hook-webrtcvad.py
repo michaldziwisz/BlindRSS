116760b59ff667181387789cc5ac3cadd4a6cf5b
Michał Dziwisz <michal@dziwisz.net>
2026-01-04 14:53:16 +0100

    fix(build): add PyInstaller hook for webrtcvad-wheels

---
+++ hooks/hook-webrtcvad.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from PyInstaller.utils.hooks import copy_metadata
+
+hiddenimports = ["webrtcvad"]
+
+datas = []
+
+# The project uses `webrtcvad-wheels` which provides the `webrtcvad` module but
+# stores distribution metadata under the `webrtcvad-wheels` name.
+#
+# The default hook in pyinstaller-hooks-contrib tries to `copy_metadata("webrtcvad")`,
+# which fails with PackageNotFoundError when only `webrtcvad-wheels` is installed.
+for dist_name in ("webrtcvad", "webrtcvad-wheels", "webrtcvad_wheels"):
+    try:
+        datas += copy_metadata(dist_name)
+        break
+    except Exception:
+        pass
+
